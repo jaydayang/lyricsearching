727b9b5b94552bda14faeb020ec78933f277869f
lbyy030 <44655934+lbyy030@users.noreply.github.com>
2019-04-04 15:46:16 +0200

    ModifyFirebase

--- src/Config/Fire.js
+++ src/Config/Fire.js
@@ -1,21 +1,21 @@
 import firebase from "firebase";
 
-const config = {
-  apiKey: "AIzaSyDvPAPkBkMNwv1tsLFxdsQBFfeWVVTSQr4",
-  authDomain: "whats-the-lyric.firebaseapp.com",
-  databaseURL: "https://whats-the-lyric.firebaseio.com",
-  projectId: "whats-the-lyric",
-  storageBucket: "whats-the-lyric.appspot.com",
-  messagingSenderId: "24922569831"
+// const config = {
+//   apiKey: "AIzaSyDvPAPkBkMNwv1tsLFxdsQBFfeWVVTSQr4",
+//   authDomain: "whats-the-lyric.firebaseapp.com",
+//   databaseURL: "https://whats-the-lyric.firebaseio.com",
+//   projectId: "whats-the-lyric",
+//   storageBucket: "whats-the-lyric.appspot.com",
+//   messagingSenderId: "24922569831"
+// };
+
+var config = {
+  apiKey: "AIzaSyCMfWQJ22mYosQd0FFUexU1sDZJvtgHy14",
+  authDomain: "lyrics-searching.firebaseapp.com",
+  databaseURL: "https://lyrics-searching.firebaseio.com",
+  projectId: "lyrics-searching",
+  storageBucket: "lyrics-searching.appspot.com",
+  messagingSenderId: "179074087016"
 };
 const fire = firebase.initializeApp(config);
 export default fire;
-
-// var config = {
-//     apiKey: "AIzaSyCMfWQJ22mYosQd0FFUexU1sDZJvtgHy14",
-//     authDomain: "lyrics-searching.firebaseapp.com",
-//     databaseURL: "https://lyrics-searching.firebaseio.com",
-//     projectId: "lyrics-searching",
-//     storageBucket: "lyrics-searching.appspot.com",
-//     messagingSenderId: "179074087016"
-//   };
