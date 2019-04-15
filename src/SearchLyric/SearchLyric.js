54dfaeb2a163b9c0dd3e8d016ecda6728497e423
jaydayang <chs@n136-p212.eduroam.kth.se>
2019-04-15 22:35:52 +0200

    Finally!

--- src/SearchLyric/SearchLyric.js
+++ src/SearchLyric/SearchLyric.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import SuggestionSidebar from "../SuggestionSidebar/SuggestionSidebar";
 import SearchResults from "../SearchResults/SearchResults";
-// import SimpleFavorite from "../SimpleFavoriteList/SimpleFavortieList";
+ import SimpleFavorite from "../SimpleFavoriteList/SimpleFavortieList";
 import { Container, Row, Col } from "reactstrap";
 import "bootstrap/dist/css/bootstrap.css";
 import "./SearchLyric.css";
@@ -58,7 +58,7 @@ class SearchLyric extends Component {
               {artistList}
             </Col>
             <Col md="4" xs="12">
-              {/* <SimpleFavorite /> */}
+             <SimpleFavorite />
               <SuggestionSidebar />
             </Col>
           </Row>
